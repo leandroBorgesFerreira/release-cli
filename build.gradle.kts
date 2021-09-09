43bbdb7bcc24d482804b56ff27f52d66072e12ac
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-09 20:06:09 -0300

    Initial setup

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.5.21"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+//application {
+//    mainClass.set("Hello")
+//}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.github.ajalt.clikt:clikt:3.2.0")
+}
