fdfe35a22aa6d7644573e3012be0c13dfe2c8e82
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-13 10:33:22 -0300

    Adding subcommands

--- src/main/kotlin/GenerateRelease.kt
+++ src/main/kotlin/GenerateRelease.kt
@@ -1,4 +1,5 @@
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.types.file
 import markdown.clean
@@ -7,15 +8,26 @@ import markdown.parser.parseReleaseDocument
 import output.print
 
 class GenerateRelease : CliktCommand() {
+    override fun run() = Unit
+}
+
+class ParseUnreleadChangelog: CliktCommand(help="Parses the unreleased changeg .md file")  {
     private val file by argument(help="Path of file").file(mustBeReadable = true, mustExist = true)
 
     override fun run() {
-        echo("File path: ${file.absolutePath}\n")
-
         parseReleaseDocument(file)
             .clean()
             .print(StdoutPrinter())
     }
 }
 
-fun main(args: Array<String>) = GenerateRelease().main(args)
+class KtlintSelected: CliktCommand(help="Applies ktlint only in the selected modules ")  {
+
+    override fun run() {
+        echo("To implement")
+    }
+}
+
+fun main(args: Array<String>) = GenerateRelease()
+    .subcommands(ParseUnreleadChangelog(), KtlintSelected())
+    .main(args)
